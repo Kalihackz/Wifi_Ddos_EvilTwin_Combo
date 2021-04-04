386e2a4222d2a821bfb231060b2201bf4cafc818
Abir Ghosh <64020453+Kalihackz@users.noreply.github.com>
2021-04-04 17:04:31 +0530

    Delete Ddos_Wifi directory

--- Ddos_Wifi/off_wifiDos.sh
+++
@@ -1,6 +0,0 @@
-printf "Reverting back to original MAC Address\nPlease Wait..."
-ifconfig wlan1 down
-sudo macchanger -p wlan1 | grep "Permanent MAC"
-iwconfig wlan1 mode managed
-ifconfig wlan1 up
-service NetworkManager restart
